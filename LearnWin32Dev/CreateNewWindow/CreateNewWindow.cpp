48ad4598928d64efecff08e1468c3068d31394d4
aiwebops <lishuaishuai_12@163.com>
2021-09-05 18:29:52 +0800

    Stage0 Learn Windows API

---
+++ LearnWin32Dev/CreateNewWindow/CreateNewWindow.cpp
@@ -0,0 +1,41 @@
+﻿#include <windows.h>
+
+// 窗口处理函数（自定义，处理消息）
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msgID, WPARAM wParam, LPARAM lParam) {
+	switch (msgID)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msgID, wParam, lParam);
+}
+// 入口函数
+int CALLBACK WinMain(HINSTANCE hIns, HINSTANCE hPreIns, LPSTR lpCmdLine, int nCmdShow) {
+	// 注册窗口类
+	WNDCLASS wc = { 0 };
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = NULL;
+	wc.hInstance = NULL;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = static_cast<LPCWSTR>(TEXT("Main"));
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&wc); // 将以上的赋值全部写入操作系统
+	// 在内存中创建窗口
+	HWND hWnd = CreateWindow(static_cast<LPCTSTR>(TEXT("Main")), static_cast<LPCTSTR>(TEXT("window")), WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hIns, NULL);
+	// 显示窗口
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	// 消息循环
+	MSG nMsg = { 0 };
+	while (GetMessage(&nMsg, NULL, 0, 0)) {
+		TranslateMessage(&nMsg);
+		DispatchMessage(&nMsg);  // 将消息交给窗口处理函数来处理
+	}
+	return 0;
+}
+
+
---
+++ LearnWin32Dev/CreateWinSys/CreateWinSys.cpp
@@ -0,0 +1,27 @@
+﻿#include <windows.h>
+
+// 窗口处理函数（自定义，处理消息）
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msgID, WPARAM wParam, LPARAM lParam) {
+	return DefWindowProc(hWnd, msgID, wParam, lParam);
+}
+// 入口函数
+int CALLBACK WinMain(HINSTANCE hIns, HINSTANCE hPreIns, LPSTR lpCmdLine, int nCmdShow) {
+	
+	HWND hWnd = CreateWindow(static_cast<LPCTSTR>(TEXT("Button")), 
+		static_cast<LPCTSTR>(TEXT("This is a SysBuildInWindow")), 
+		WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hIns, NULL);
+	// 显示窗口
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	// 消息循环
+	MSG nMsg = { 0 };
+	while (GetMessage(&nMsg, NULL, 0, 0)) {
+		TranslateMessage(&nMsg);
+		DispatchMessage(&nMsg);  // 将消息交给窗口处理函数来处理
+	}
+	return 0;
+}
+
+
+
+
